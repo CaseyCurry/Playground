f4ee8d2b7e9bfb1d45b7bcbc15dbc8c96da43006
Casey Curry <casey.de.curry@gmail.com>
2017-01-19 11:05:14 -0600

    start exploring GoCD

---
+++ gocd/readme.txt
@@ -0,0 +1,1 @@
+Demo project to explore Thoughtworks' GoCD toolset.
---
+++ gocd/src/unit/test.js
@@ -0,0 +1,10 @@
+const expect = require("chai")
+  .expect;
+
+describe("unit test suite", () => {
+  it("should run a test", () => {
+    expect(true)
+      .to
+      .equal(true);
+  });
+});
