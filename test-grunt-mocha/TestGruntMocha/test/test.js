ac566646d89118cafcb82c55b1232a566d628503
Casey Curry <casey.de.curry@gmail.com>
2018-06-04 13:18:10 -0500

    cleanup

--- test-grunt-mocha/TestGruntMocha/test/test.js
+++
@@ -1,7 +0,0 @@
-﻿const expect = require("chai").expect;
-
-describe("Test Suite", function () {
-    it("Should Control Build Status", function () {
-        expect(true).to.equal(false);
-    });
-});
